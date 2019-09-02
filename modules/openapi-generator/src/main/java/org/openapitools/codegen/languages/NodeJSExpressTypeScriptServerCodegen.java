c843b534bde1ad38739d597b93585c2ab5265572
Marcin Nowak <ian@sonyx.net>
2019-09-02 15:53:51 +0200

    something actually works!

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/NodeJSExpressTypeScriptServerCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/NodeJSExpressTypeScriptServerCodegen.java
@@ -79,21 +79,21 @@ public class NodeJSExpressTypeScriptServerCodegen extends DefaultCodegen impleme
         apiTemplateFiles.put("controller.mustache", ".ts");
         apiTemplateFiles.put("service.mustache", ".ts");
 
-        supportingFiles.add(new SupportingFile("openapi.mustache", "api", "openapi.yaml"));
+        supportingFiles.add(new SupportingFile("openapi.mustache", "../../", "openapi.yaml"));
         //supportingFiles.add(new SupportingFile("config.mustache", "", "config.js"));
         //supportingFiles.add(new SupportingFile("expressServer.mustache", "", "expressServer.js"));
         //supportingFiles.add(new SupportingFile("index.mustache", "", "index.js"));
-        supportingFiles.add(new SupportingFile("logger.mustache", "", "logger.js"));
+        //supportingFiles.add(new SupportingFile("logger.mustache", "", "logger.js"));
         supportingFiles.add(new SupportingFile("eslintrc.mustache", "", ".eslintrc.json"));
 
         // utils folder
         supportingFiles.add(new SupportingFile("utils" + File.separator + "openapiRouter.mustache", "utils", "openapiRouter.ts"));
 
         // controllers folder
-        //supportingFiles.add(new SupportingFile("controllers" + File.separator + "index.mustache", "controllers", "index.js"));
+        supportingFiles.add(new SupportingFile("controllers" + File.separator + "index.mustache", "controllers", "index.ts"));
         supportingFiles.add(new SupportingFile("controllers" + File.separator + "Controller.mustache", "controllers", "Controller.ts"));
         // service folder
-        //supportingFiles.add(new SupportingFile("services" + File.separator + "index.mustache", "services", "index.js"));
+        supportingFiles.add(new SupportingFile("services" + File.separator + "index.mustache", "services", "index.ts"));
         supportingFiles.add(new SupportingFile("services" + File.separator + "Service.mustache", "services", "Service.ts"));
 
         // do not overwrite if the file is already present
